22895b7b9397da9d52ae61cc7c58472df53f01af
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-09-06 00:12:54 -0300

    Some documentation

--- ThreadSum/Models/MatrixModel.cs
+++ ThreadSum/Models/MatrixModel.cs
@@ -7,13 +7,29 @@ namespace ThreadSum.Models;
 /// This is a bi-dimensional matrix
 /// </summary>
 public class MatrixModel {
+	#region Atrributes
 	private Cell[,] _Values;
+	#endregion
+	#region Properties
+	/// <summary>
+	/// Gets the row size of the matrix
+	/// </summary>
 	public int RowCount {
 		get; private set;
 	}
+	/// <summary>
+	/// Gets the column size of the matrix
+	/// </summary>
 	public int ColumnCount {
 		get; private set;
 	}
+	#endregion
+	#region Constructors
+	/// <summary>
+	/// Creates a matrix
+	/// </summary>
+	/// <param name="rowCount">Row size</param>
+	/// <param name="columnCount">Column size</param>
 	public MatrixModel(int rowCount, int columnCount) {
 		this._Values = new Cell[this.RowCount = rowCount, this.ColumnCount = columnCount];
 		Random random = new();
@@ -23,8 +39,17 @@ public class MatrixModel {
 			}
 		}
 	}
+	#endregion
+	#region Indexers
+	/// <summary>
+	/// Gets a cell from the matrix
+	/// </summary>
+	/// <param name="row">Row index</param>
+	/// <param name="column">Column index</param>
+	/// <returns>Cell</returns>
 	public Cell this[int row, int column] {
 		get => this._Values[row, column];
 		set => this._Values[row, column] = value;
 	}
+	#endregion
 }
